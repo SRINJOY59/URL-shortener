7e56694a719bc57cfdf9ea2aae3ddb2d9025b7b1
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-01-25 01:11:47 +0530

    Kalidas-Updated-Home-page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,15 @@ import logo from '../../assets/Logo.svg'
 export const Navbar = () => {
   return (
     <nav className='w-full bg-transparent z-50 flex py-6 bg-[#0B101B] justify-between items-center navbar'>
-        <img src={logo} className='ml-10'/>
+        <p className='ml-10 text-[40px] text-transparent bg-gradient-to-r from-bright-pink to-bright-blue bg-clip-text font-bold justify-center z-50 center'>Shorty-URL</p>
         <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-            <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mr-5">
-              <button className="bg-dark-gray hover:bg-blue-500 text-white h-[50px] w-[125px] font-semibold hover:text-white py-2 px-8 border border-blue-500 hover:border-transparent rounded-full">
-              Login
+            <li className="font-poppins font-bold cursor-pointer text-[16px] text-white mr-5">
+              <button className="bg-dark-gray font-bold hover:bg-bright-blue text-lite-gray h-[50px] w-[125px] hover:text-white py-2 px-8 border-3 border-elite-black hover:border-transparent rounded-full">
+              Log In
               </button>
             </li>
             <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mr-5">
-              <button className="bg-bright-blue h-[50px] w-[140px]  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              <button className="bg-bright-blue shadow-bright-blue h-[50px] w-[150px] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                 Register Now
               </button>
             </li>
