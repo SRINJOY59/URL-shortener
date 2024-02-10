58125b2e090292e145a7557d974dc63d590d12a9
SRINJOY59 <119725096+SRINJOY59@users.noreply.github.com>
2024-02-10 23:56:54 +0530

    files updated

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './Components/signUp';
 import SignIn from './Components/signIn';
-import Dashboard from './Components/Dashboard';
+import './style.css'
 import UserProfile from './Components/UserProfile';
 
 function App() {
@@ -16,7 +16,6 @@ function App() {
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/" element={<SignUp />} /> 
           <Route path="/user" element={<UserProfile/>} />
-          <Route path="/dashboard" element={< Dashboard/>} />
         </Routes>
       </Router>
     </div>
